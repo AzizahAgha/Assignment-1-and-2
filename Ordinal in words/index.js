5ecb5c2ab1e3265583315d8540e099004501f2f8
Azizah Agha <117432933+AzizahAgha@users.noreply.github.com>
2022-11-06 03:28:07 +0300

    Add files via upload

---
+++ Ordinal in words/index.js
@@ -0,0 +1,40 @@
+ one = [ "", " first ", " second ", " third ", " fourth ",
+                 " fifth ", "sixth ", "seventh", " eightth ",
+                 " nineth ", "tenth ", "eleventh ", " twelvth ",
+                 " mthirteenth ", " fourteenth ", " fifteenth ",
+                 "msixteenth ", " seventeenth ", " eighteenth ",
+                 " nineteenth " ];
+
+ ten = [ "", "", "twenty", "thirty", "forty",
+                 "fifty", "sixty", "seventy", "eighty",
+                 "ninety" ];
+
+
+function ordToWords(n){
+    var str = "";
+
+    if(n%10==0){
+        str += ten[Math.floor(n /10)]+"th";
+    }
+    else if (n > 19){
+        str += ten[Math.floor(n /10)] + one[n % 10];
+      }
+
+    else
+    {
+        str += one[n];
+    }
+
+
+
+    return str;
+    console.log(str);
+}
+
+
+$("button").click(function(){
+  var n = document.getElementById("input").value;
+  document.getElementById("ordinal").innerHTML = n;
+
+ $("p").text("The ordinal number of " +n+ " is : " + ordToWords(n));
+});
